54723f73c6e31df6ef892ca9b526eb01a603abf9
Alya <masterparser1@gmail.com>
2026-08-03 02:26:30 +0700

    Remove temporary migration API (cutover complete)

--- src/api/migration/routes/migration.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = {
-  routes: [
-    { method: 'GET',  path: '/migration/export', handler: 'migration.export', config: { auth: false, policies: [], middlewares: [] } },
-    { method: 'POST', path: '/migration/import', handler: 'migration.import', config: { auth: false, policies: [], middlewares: [] } },
-    { method: 'GET',  path: '/migration/stats',  handler: 'migration.stats',  config: { auth: false, policies: [], middlewares: [] } },
-  ],
-};
